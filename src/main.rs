7e6ad7d5c5f062712a63ecbe651c7f119f0126da
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-12-19 21:31:18 -0800

    Added structopt logic for processing CLI input for output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use figlet_rs::FIGfont;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct CLI {
+    content: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Obtain CLI Arguments
+    let args = CLI::from_args();
+
+    // Set font for figlet program
+    let standard_font = FIGfont::standand().unwrap();
+
+    // Get text content from CLI args
+    let figure = standard_font.convert(&args.content);
+    assert!(figure.is_some());
+
+    // Display figlet output
+    println!("{}", figure.unwrap());
 }
